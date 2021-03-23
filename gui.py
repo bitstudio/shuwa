0ded2f5220765281aa23eafc958b8411aa316472
bit-dome <dome@thebitstudio.com>
2021-03-23 17:19:25 +0700

    Disable tab change when no database.

--- gui.py
+++ gui.py
@@ -13,37 +13,28 @@ class DemoGUI:
         # self.root.protocol('WM_DELETE_WINDOW', self.close_all)
         self.root.geometry("400x150")
         
-        self.TAB_CONTROL = ttk.Notebook(self.root)
-
-        # self.TAB0 = Frame(self.TAB_CONTROL, bg="#00766c")
-        # self.TAB_CONTROL.add(self.TAB0, text='Process videos')
-        # self.TAB_CONTROL.bind("<<NotebookTabChanged>>", self.change_mode_on_tab)
+        self.notebook = ttk.Notebook(self.root)              
         
-        
-        self.TAB1 = Frame(self.TAB_CONTROL, bg="#00766c")
-        self.TAB_CONTROL.add(self.TAB1, text='Record')
-        self.TAB_CONTROL.bind("<<NotebookTabChanged>>", self.change_mode_on_tab)
+        self.TAB1 = Frame(self.notebook, bg="#00766c")
+        self.notebook.add(self.TAB1, text='Record')
+        self.notebook.bind("<<NotebookTabChanged>>", self.change_mode_on_tab)
         
 
-        self.TAB2 = Frame(self.TAB_CONTROL, bg="#00766c")
-        self.TAB_CONTROL.add(self.TAB2, text='Play mode')
-        self.TAB_CONTROL.pack(expand=2, fill="both")
+        self.TAB2 = Frame(self.notebook, bg="#00766c")
+        self.notebook.add(self.TAB2, text='Play mode')
+        self.notebook.pack(expand=2, fill="both")
         
         
         
         self.is_play_mode = 0
         self.is_recording = False
     
-    
-        # # ─── PRCESS VIDEOS MODE ─────────────────────────────────────────────────
-        # self.record_button_text = StringVar(self.TAB0, name="record_button_text")
-        # self.record_button_text.set("Open")
 
         # ─── RECORD MODE ─────────────────────────────────────────────────
              
         # record button.     
         self.record_button_text = StringVar(self.TAB1, name="record_button_text")
-        self.record_button_text.set("Record")
+        self.record_button_text.set("Record (R)")
         self.is_recording = False
         record_button = Button(self.TAB1, textvariable=self.record_button_text,
                                command=self.toggle_record_button,
@@ -62,7 +53,7 @@ class DemoGUI:
         
         
         # save button.
-        self.save_button = Button(self.TAB1, text="Save", command=self.save_template)
+        self.save_button = Button(self.TAB1, text="Save", command=self.save_database)
         self.save_button.grid(row=2, column=1, sticky=W)
         
         
@@ -71,7 +62,7 @@ class DemoGUI:
         
         # record button.     
         self.record_button_text = StringVar(self.TAB2, name="record_button_text")
-        self.record_button_text.set("Record")
+        self.record_button_text.set("Record (R)")
         self.is_recording = False
         record_button_p = Button(self.TAB2, textvariable=self.record_button_text,
                                command=self.toggle_record_button,
@@ -90,29 +81,28 @@ class DemoGUI:
         
     def toggle_record_button(self):        
         self.is_recording = not self.is_recording
-        tab_id = self.TAB_CONTROL.index(self.TAB_CONTROL.select())
+        tab_id = self.notebook.index(self.notebook.select())
         if self.is_recording:
-            self.record_button_text.set("Stop")                       
-            self.TAB_CONTROL.tab(not tab_id, state="disabled")
+            self.record_button_text.set("Stop (R)")                       
+            self.notebook.tab(not tab_id, state="disabled")
             self.name_box["state"] = DISABLED
             self.save_button["state"] = DISABLED
 
         else:
-            self.record_button_text.set("Record")
-            self.TAB_CONTROL.tab(not tab_id, state="normal")
+            self.record_button_text.set("Record (R)")
+            self.notebook.tab(not tab_id, state="normal")
             self.name_box["state"] = NORMAL
             self.save_button["state"] = NORMAL           
      
-            
-        print("button click", self.is_recording)
+
         
             
-    def change_mode_on_tab(self, event):
-        self.is_play_mode = self.TAB_CONTROL.index("current")
-        print("mode changed.", self.is_play_mode)
+    def change_mode_on_tab(self, event):    
+        self.is_play_mode = self.notebook.index("current")
         
         
-    def save_template(self):    
+        
+    def save_database(self):    
         print("save", self.name_box.get())
         
         
--- webcam_demo_knn.py
+++ webcam_demo_knn.py
@@ -2,7 +2,7 @@ import sys, os
 import glob
 import time
 import cv2
-
+from datetime import datetime
 import numpy as np
 from common import crop_square
 from constants import *
@@ -29,19 +29,27 @@ class Application(DemoGUI, Pipeline):
 
 
     def load_database(self):
+        print("[INFO] Reading database...")
         self.database = []
         self.labels= []
-        all_folder = glob.glob(os.path.join(KNN_DATASET_PATH, "*")) 
-    
+        all_folder = glob.glob(os.path.join(KNN_DATASET_PATH, "*"))
+           
         for folder in all_folder:
             all_file = glob.glob(os.path.join(folder, "*.txt"))
             class_label = os.path.split(folder)[-1]
             for f in all_file:
                 self.database.append(np.loadtxt(f))
                 self.labels.append(class_label)
-            
-        self.database = np.stack(self.database)
-        self.labels = np.array(self.labels)
+                
+        if len(self.database) == 0:
+            print("[INFO] Can't load any database file. Try record any database first.")           
+            self.notebook.select(0)
+            return False
+        
+        else:    
+            self.database = np.stack(self.database)
+            self.labels = np.array(self.labels)            
+            return True
 
         
 
@@ -55,16 +63,16 @@ class Application(DemoGUI, Pipeline):
             self.toggle_record_button()     
 
           
-    def change_mode_on_tab(self, event):
-        super().change_mode_on_tab(event)
- 
-        self.result_class_name = ""
+    def change_mode_on_tab(self, event):         
+        # check database before change from record mode to play mode.
+        if not self.is_play_mode:
+            can_change_mode = self.load_database()
         
-        if self.is_play_mode:
-            print("[INFO] Entered play mode.")        
-            print("[INFO] Reading database...")
-            self.load_database()
-      
+        if can_change_mode:
+            print("[INFO] Entering play mode.")        
+            self.result_class_name = ""
+            super().change_mode_on_tab(event)
+            
             
     def toggle_record_button(self):
         super().toggle_record_button()
@@ -73,11 +81,10 @@ class Application(DemoGUI, Pipeline):
             if len(self.pose_history) > NUM_FRAME_SAMPLES:
                 # playmode
                 if self.is_play_mode:
-                    result_class_name, total_dist, pose_disttance, face_distance, hands_distance = self.run_knn_classifier()
+                    result_class_name = self.run_knn_classifier()
                     self.console_box.delete('1.0', 'end')
                     self.console_box.insert('end',
-                        "Nearest class: {:s} - {:.2f}\n| Pose distance: {:.2f}\n| Face distance: {:.2f}\n| Hands distance: {:.2f}"
-                        .format(result_class_name, total_dist, pose_disttance, face_distance, hands_distance))
+                        "Nearest class: {:s}\n".format(result_class_name))
                                 
                         
                 # record mode.
@@ -94,10 +101,13 @@ class Application(DemoGUI, Pipeline):
         
                 
  
-    def save_template(self):
-        super().save_template()
-        
-        # read texbox entry.
+    def save_database(self):
+        """
+        Save recorded templates to files.
+        """       
+        super().save_database()        
+        timestamp =  datetime.now().strftime("%d%m%Y%H%M%S")
+        # Read texbox entry, use as folder name.
         if self.name_box.get() != "" and len(self.records) > 0:                
             folder_name = self.name_box.get()            
             folder_path = os.path.join(KNN_DATASET_PATH, folder_name)
@@ -105,9 +115,10 @@ class Application(DemoGUI, Pipeline):
             if not os.path.exists(folder_path):
                 os.mkdir(folder_path)
             
-            for i, a in enumerate(self.records):      
-                np.savetxt(folder_path+"\\"+str(i)+".txt", a)
-            print("[INFO] Template saved.")
+            for i, a in enumerate(self.records):     
+                
+                np.savetxt(folder_path+os.sep+timestamp+str(i)+".txt", a)
+            print("[INFO] database saved.")
             # clear.
             self.records = []
             self.num_records_text.set("num records: "+ str(len(self.records)))            
@@ -127,7 +138,7 @@ class Application(DemoGUI, Pipeline):
 
 
         t2 = time.time() - t1
-        cv2.putText(frame_rgb, "FPS: {:.0f}".format(1 / t2), (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (203, 52, 247), 1)
+        cv2.putText(frame_rgb, "{:.0f} ms".format(t2*1000), (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (203, 52, 247), 1)
         self.show_frame(frame_rgb)        
 
         self.root.after(1, self.video_loop)
